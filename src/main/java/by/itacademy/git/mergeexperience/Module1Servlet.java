eb2e620bb45c295eb44e372f2138eab2c349aaae
Andrei Borisov <andrei.borr@gmail.com>
2023-03-23 12:50:59 +0300

    Project added

---
+++ src/main/java/by/itacademy/git/mergeexperience/Module1Servlet.java
@@ -0,0 +1,42 @@
+package by.itacademy.git.mergeexperience;
+
+import java.io.*;
+
+import by.itacademy.git.mergeexperience.topics.Module1Topics;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/module1")
+public class Module1Servlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Модуль 1";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+
+        out.println("<table>");
+        for (Module1Topics value : Module1Topics.values())
+            out.println("<tr><td>"
+                    + value.getOrder() + "</td><td>"
+                    + value.getTopic() + "</td><td>"
+                    + value.getDesc()
+                    + "</td><td><a href='not_implemented_yet.html'>Подробнее</a>"
+                    + "</td></tr>");
+
+        out.println("</table>");
+
+        out.println("<a href='/'>Назад</a>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/java/by/itacademy/git/mergeexperience/topics/Module1Topics.java
@@ -0,0 +1,31 @@
+package by.itacademy.git.mergeexperience.topics;
+
+public enum Module1Topics {
+    JAKARTA(1, "Jakarta", "Обзор java enterprise технологий"),
+    MAVEN(2, "Apache Maven", "Изучение сборщика проектов Maven"),
+    TOMCAT(3, "Apache Tomcat", "Изучение основ HTTP, TCP/IP и web сервера Tomcat"),
+    SERVLET(4, "HttpServlet", "Освоение технологии java сервлетов"),
+    GIT(5, "GIT", "Работа с системой контроля версий GIT");
+
+    private int order;
+    private String topic;
+    private String desc;
+
+    Module1Topics(int order, String topic, String desc) {
+        this.order = order;
+        this.topic = topic;
+        this.desc = desc;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
